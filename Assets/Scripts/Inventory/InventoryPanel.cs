09c10aefe3c13a857b1cd4599b7a3b06b1b5343e
HumbleToymakerGames <humbletoymakergames@gmail.com>
2022-11-04 16:39:59 -0600

    Added more inventory code

---
+++ Assets/Scripts/Inventory/InventoryPanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPanel : MonoBehaviour
+{
+    [SerializeField] ItemContainer inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+
+    }
+}
