e937536ff2515a0153ab558da334d6d29c95c4fd
harrison531 <harrisongardner531@gmail.com>
2022-11-09 17:44:48 -0700

    Replaced old ovens on map with new prefab

--- Assets/Scripts/World/Tile Interaction/PlaceableObject.cs
+++ Assets/Scripts/World/Tile Interaction/PlaceableObject.cs
@@ -14,6 +14,7 @@ public class PlaceableObject : MonoBehaviour
 
     private Tilemap groundTileMap;
 
+    public bool flipped = false;
 
     /// <summary>
     /// Places an object where it is at currently
@@ -58,11 +59,90 @@ public class PlaceableObject : MonoBehaviour
         MapInformation.SetTileType(groundTileMap.WorldToCell(transform.position - new Vector3(0, transform.localScale.y / 2, 0)), tileType);
     }
 
+    public void SetComponents(PlaceableObjectSO placeableObjectSO)
+    {
+        WorldAppliance worldAppliance;
+        this.placeableObjectSO = placeableObjectSO;
+
+        gameObject.GetComponent<SpriteRenderer>().sprite = placeableObjectSO.sprite;
+
+        if (placeableObjectSO.type == TileType.Interactable)
+        {
+            //If the object is interactable add components needed to be interactable
+            if (gameObject.GetComponent<Rigidbody2D>() == null)
+            {
+                gameObject.AddComponent<Rigidbody2D>();
+                Rigidbody2D rb2d = gameObject.GetComponent<Rigidbody2D>();
+                rb2d.bodyType = RigidbodyType2D.Static;
+            }
+            if (gameObject.GetComponent<CircleCollider2D>() == null)
+            {
+                gameObject.AddComponent<CircleCollider2D>();
+                CircleCollider2D cc2d = gameObject.GetComponent<CircleCollider2D>();
+                cc2d.radius = 0.41f;
+            }
+            if (gameObject.GetComponent<WorldAppliance>() == null)
+            {
+                gameObject.AddComponent<WorldAppliance>();
+                worldAppliance = gameObject.GetComponent<WorldAppliance>();
+            }
+
+            worldAppliance = gameObject.GetComponent<WorldAppliance>();
+            worldAppliance.applianceSO = placeableObjectSO.applianceSOIfApplicable;
+            if (!flipped)
+            {
+                worldAppliance.usePositionOffset = placeableObjectSO.usePositionOffsetIfApplicable;
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                worldAppliance.usePositionOffset = new Vector3Int(placeableObjectSO.usePositionOffsetIfApplicable.y, placeableObjectSO.usePositionOffsetIfApplicable.x, placeableObjectSO.usePositionOffsetIfApplicable.z);
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        else
+        {
+            if (gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                Destroy(gameObject.GetComponent<Rigidbody2D>());
+            }
+            if (gameObject.GetComponent<CircleCollider2D>() != null)
+            {
+                Destroy(gameObject.GetComponent<CircleCollider2D>());
+            }
+            if (gameObject.GetComponent<WorldAppliance>() != null)
+            {
+                Destroy(gameObject.GetComponent<WorldAppliance>());
+            }
+        }
+    }
+
+    public void FlipObject(bool flipped)
+    {
+        WorldAppliance worldAppliance = gameObject.GetComponent<WorldAppliance>();
+        this.flipped = flipped;
+        if (!flipped)
+        {
+            if(tileType == TileType.Interactable)
+                worldAppliance.usePositionOffset = placeableObjectSO.usePositionOffsetIfApplicable;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            if (tileType == TileType.Interactable)
+                worldAppliance.usePositionOffset = new Vector3Int(placeableObjectSO.usePositionOffsetIfApplicable.y, placeableObjectSO.usePositionOffsetIfApplicable.x, placeableObjectSO.usePositionOffsetIfApplicable.z);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        Debug.Log("Flip");
+    }
+
     public void Update()
     {
         if (placeOnStart && !placed)
         {
             SnapToGrid.Snap(transform);
+            SetComponents(placeableObjectSO);
             PlaceObject();
         }
         if (placed == false && Player.controlState != ControlState.Edit)
