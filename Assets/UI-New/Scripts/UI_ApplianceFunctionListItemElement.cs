10ddae987cd421e8ad0bd416c74101bc7c6c05a8
Matt Henry <matt@henrymatt.com>
2022-11-11 14:42:19 -0700

    Add InputItem sprites to ApplianceFunctionListItem

--- Assets/UI-New/Scripts/UI_ApplianceFunctionListItemElement.cs
+++ Assets/UI-New/Scripts/UI_ApplianceFunctionListItemElement.cs
@@ -9,6 +9,8 @@ public class UI_ApplianceFunctionListItemElement : MonoBehaviour
     public TextMeshProUGUI applianceFunctionNameText;
     public TextMeshProUGUI applianceFunctionDurationText;
     public GameObject outputProduct;
+    public GameObject inputItemsParent;
+    public GameObject singleInputItemPrefab;
 
     public Button startFunctionButton;
 
@@ -19,5 +21,29 @@ public class UI_ApplianceFunctionListItemElement : MonoBehaviour
         applianceFunctionDurationText.text = applianceFunction.GetDurationString();
         outputProduct.GetComponent<UI_ItemQuantityElement>().SetToItemQuantity(outputItem);
         startFunctionButton.onClick.AddListener(() => applianceFunction.parentAppliance.FunctionClicked(applianceFunction));
+        foreach(Transform el in inputItemsParent.transform)
+        {
+            Destroy(el.gameObject);
+        }
+
+        foreach (Sprite sprite in GetInputItemsSprites(applianceFunction))
+        {
+            GameObject inputItemEl = Instantiate(singleInputItemPrefab, inputItemsParent.transform);
+            inputItemEl.GetComponent<UI_InventoryItemSingleElement>().SetSprite(sprite);
+        } 
+    }
+
+    public List<Sprite> GetInputItemsSprites(ApplianceFunction applianceFunction)
+    {
+        List<Sprite> spriteList = new List<Sprite>();
+        List<SlotClass> inputItems = applianceFunction.GetItemQuantitiesForInputs();
+        foreach(SlotClass inputItem in inputItems)
+        {
+            for(int i = 0; i < inputItem.GetQuantity(); i++)
+            {
+                spriteList.Add(inputItem.GetItem().itemIcon);
+            }
+        }
+        return spriteList;
     }
 }
