973247f06b85570c7b8cafc92abdd6953f7139d6
HumbleToymakerGames <humbletoymakergames@gmail.com>
2022-11-17 17:25:47 -0700

    Shop setup

--- Assets/Scripts/InventoryShop/BuyItemSO.cs
+++ Assets/Scripts/InventoryShop/BuyItemSO.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Shop", menuName = "Shop/BuyItem")]
 public class BuyItemSO : ScriptableObject
 {
-    public List<SlotClass> outputItems = new List<SlotClass>();
-    //public SlotClass outputItem;
+    //public List<SlotClass> addItems = new List<SlotClass>();
+    public SlotClass addItem;
+    public SlotClass subtractItem;
 
-   
+    public void AddItemToCart(ShopFunction shopFunction)
+    {
+        shopFunction.Add(addItem.GetItem(), addItem.GetQuantity());
+    }
+
+    public void RemoveItemFromCart(ShopFunction shopFunction)
+    {
+        shopFunction.Remove(subtractItem.GetItem(), subtractItem.GetQuantity());
+    }
 }
